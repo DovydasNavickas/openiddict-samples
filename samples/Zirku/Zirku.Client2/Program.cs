d6ea69218e113f9d657651782ae3160bae67efb9
Dovydas Navickas <dovydas@reactway.com>
2024-08-16 03:49:11 +0300

    Changed Zirku.Client2 ports to match the ones in the server. Added default redirect to index.html.

--- samples/Zirku/Zirku.Client2/Program.cs
+++ samples/Zirku/Zirku.Client2/Program.cs
@@ -1,4 +1,10 @@
 var builder = WebApplication.CreateBuilder(args);
+
 var app = builder.Build();
+
 app.UseStaticFiles();
+
+// Default redirect to the index page
+app.MapGet("/", () => Results.Redirect("/index.html"));
+
 app.Run();
